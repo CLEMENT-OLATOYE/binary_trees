addb1d3eceb62f4549c5e5d5dc6a0c848535c7e8
Shadoolee <oluwaseundvd@gmail.com>
2024-03-29 18:47:56 -0700

    Binary Trees

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,63 +1,34 @@
 #include "binary_trees.h"
 
-
 /**
-
  * binary_tree_rotate_right - Right-rotates a binary tree.
-
  * @tree: A pointer to the root node of the tree to rotate.
-
  *
-
  * Return: A pointer to the new root node after rotation.
-
  */
-
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
-
 {
+	binary_tree_t *pivot, *tmp;
 
-        binary_tree_t *driver, *tmp;
+	if (tree == NULL || tree->left == NULL)
+		return (NULL);
 
+	pivot = tree->left;
+	tmp = pivot->right;
+	pivot->right = tree;
+	tree->left = tmp;
+	if (tmp != NULL)
+		tmp->parent = tree;
+	tmp = tree->parent;
+	tree->parent = pivot;
+	pivot->parent = tmp;
+	if (tmp != NULL)
+	{
+		if (tmp->left == tree)
+			tmp->left = pivot;
+		else
+			tmp->right = pivot;
+	}
 
-        if (tree == NULL || tree->left == NULL)
-
-                return (NULL);
-
-
-        driver = tree->left;
-
-        tmp = driver->right;
-
-        driver->right = tree;
-
-        tree->left = tmp;
-
-        if (tmp != NULL)
-
-                tmp->parent = tree;
-
-        tmp = tree->parent;
-
-        tree->parent = driver;
-
-        driver->parent = tmp;
-
-        if (tmp != NULL)
-
-        {
-
-                if (tmp->left == tree)
-
-                        tmp->left = driver;
-
-                else
-
-                        tmp->right = driver;
-
-        }
-
-
-        return (driver);
-
+	return (pivot);
 }
